2c1c1c206df4a773c84a276af2896123e0fb7c9a
Raihan115219 <aburaihan115219@gmail.com>
2024-10-22 13:41:12 +0600

    added pdftron

--- pdftronpractice/src/App.js
+++ pdftronpractice/src/App.js
@@ -3,20 +3,17 @@ import WebViewer from "@pdftron/webviewer";
 import "./App.css";
 
 function App() {
-  const viewer = useRef(null); // Reference to the div for WebViewer
-
+  const viewer = useRef(null);
   useEffect(() => {
-    // Prevent multiple instances of WebViewer from being initialized
     if (viewer.current && !viewer.current.webViewerInstance) {
       WebViewer(
         {
-          path: "/lib", // path to WebViewer lib files
+          path: "/lib",
           initialDoc:
             "https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf",
         },
         viewer.current
       ).then((instance) => {
-        // Store instance inside the ref to avoid re-initialization
         viewer.current.webViewerInstance = instance;
 
         const { documentViewer } = instance.Core;
@@ -26,7 +23,6 @@ function App() {
       });
     }
 
-    // Cleanup: Remove WebViewer instance if component is unmounted
     return () => {
       if (viewer.current && viewer.current.webViewerInstance) {
         console.log("Cleaning up WebViewer instance");
